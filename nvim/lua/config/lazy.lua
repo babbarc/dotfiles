5ce7c4ab795b258f126963a3f8b937787cc29f9e
babbarc <babbarc@home>
2023-12-20 21:29:49 +0000

    feat(extras): add experimental plugin lazyrc
    enable loading project specfic lazyvim configuration using .lazy.lua file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.extras.lazyrc" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
