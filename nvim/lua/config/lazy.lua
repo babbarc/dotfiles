020f5ee79cfc423fe11853030fdb7d741c694c9b
babbarc <babbarc@home>
2024-04-07 09:31:29 +0100

    Revert "feat(extras): add experimental plugin lazyrc"
    This reverts commit 5ce7c4ab795b258f126963a3f8b937787cc29f9e.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "plugins.extras.lazyrc" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
