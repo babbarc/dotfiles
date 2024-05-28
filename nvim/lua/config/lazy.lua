beb707a6339fb6da54891ed7f6f0756851a554b5
babbarc <babbarc@home>
2024-05-28 18:24:18 +0100

    feat(nvim): remove lazyrc extra
    lazyrc has been integrated into lazyvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "lazyvim.plugins.extras.lazyrc" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
