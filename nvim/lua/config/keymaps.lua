c67579148fc1506c8eb17ac68556db66a7a54df1
babbarc <babbarc@home>
2025-04-28 16:04:00 +0100

    feat(nvim): add interactive diffviewopen

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,6 +14,41 @@ vim.keymap.set(
   { silent = true, desc = "Add line above" }
 )
 vim.keymap.set("n", "<leader>ghB", ":ToggleBlame virtual<Enter>", { silent = true, desc = "Toggle virtual blame" })
+vim.keymap.set("n", "<leader>gd", function()
+  require("fzf-lua").fzf_exec(
+    -- 1) use --color=always so git embeds ANSI color codes in the log
+    "git log  --branches --oneline --decorate --color=always --pretty=format:'%C(yellow)%h%C(reset) %C(cyan)%d%C(reset) %s'",
+    {
+      prompt = "Commits> ",
+      fzf_opts = {
+        -- enable true ANSI-color parsing
+        ["--ansi"] = true,
+        -- allow multi-select with TAB/SPACE
+        ["--multi"] = true,
+        -- show a preview pane on the right
+        ["--preview"] = "git show --color=always {1}",
+        -- make the preview take 60% of the width and wrap long lines
+        ["--preview-window"] = "right:60%:wrap",
+        -- (optional) nicer keybindings inside FZF
+        ["--bind"] = "tab:toggle+down,shift-tab:toggle+up",
+      },
+      actions = {
+        -- default action: DiffviewOpen between two commits
+        ["default"] = function(selected)
+          if #selected ~= 2 then
+            vim.notify("Please select exactly TWO commits", vim.log.levels.ERROR)
+            return
+          end
+          local sha1 = selected[1]:match("^([a-f0-9]+)")
+          local sha2 = selected[2]:match("^([a-f0-9]+)")
+          if sha1 and sha2 then
+            vim.cmd("DiffviewOpen " .. sha2 .. ".." .. sha1)
+          end
+        end,
+      },
+    }
+  )
+end, { desc = "Diff two selected commits" })
 
 -- prefil edit window with common scenarios to avoid repeating query and submit immediately
 local prefill_edit_window = function(request)
